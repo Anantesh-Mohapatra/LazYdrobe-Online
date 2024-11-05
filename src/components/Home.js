a4e96372770fb53108db6be789858d0f48dd6498
Winni <wg2261@nyu.edu>
2024-11-05 14:02:21 -0500

    Decorated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,17 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="home-page">
-      <h1>Welcome To LazYdrobe</h1>
+      <div className="home">
+        <h1>Welcome To LazYdrobe</h1>
+        <hr class="rounded"></hr>
+        <p className="intro-text">
+          Trouble picking outfits for the upcoming days?<br/>
+          Too much clothes to keep track of?<br/>
+          Look no further. Our revolutionary app is here to assist you in keeping up with fashion trends and weather. <br/>
+          <br/>
+          Simply upload items in your wardrobe so that we can suggest future outfits based on the weather conditions and the current fashion trend for you while providing clothing suggestions to fill in the gaps in your wardrobe.
+        </p>
+      </div>
     </div>
   );
 };
