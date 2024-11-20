ab4af0045390824725267370321d40e53d40987d
Anantesh Mohapatra <ananteshmohapatra@gmail.com>
2024-11-19 23:55:14 -0500

    Added new component for realsies

---
+++ src/components/WardrobeControls.js
@@ -0,0 +1,69 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const WardrobeControls = ({
+  openItemModal,
+  openOutfitModal,
+  selectAll,
+  unselectAll,
+  filter,
+  setFilter,
+  weatherFilter,
+  setWeatherFilter,
+  deleteSelectedItems,
+  multiSelect
+}) => {
+  return (
+    <div className='on-top'>
+      <button onClick={() => openItemModal(null)} className='add-button'>Add Item</button>
+      <button onClick={() => openOutfitModal(null)} className='create-button'>Create Outfit</button>
+      <button onClick={selectAll} className='select-button'>Select All</button>
+      <button onClick={unselectAll} className='unselect-button'>Unselect All</button>
+      
+      {/* Filter */}
+      <input 
+        type="text" 
+        placeholder="Filter by clothing type" 
+        className="type-filter"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      <select 
+        value={weatherFilter} 
+        onChange={(e) => setWeatherFilter(e.target.value)}
+        className="weather-filter"
+      >
+        <option value="">Select weather filter</option>
+        <option value="All Year Around">All Year Around</option>
+        <option value="Summer">Summer</option>
+        <option value="Winter">Winter</option>
+        <option value="Rainy">Rainy</option>
+        <option value="Other">Other</option>
+      </select>
+
+      <button 
+        onClick={deleteSelectedItems} 
+        className='delete-button' 
+        disabled={multiSelect.length == 0}
+      >
+        Delete Selected
+      </button>
+    </div>
+  );
+};
+
+WardrobeControls.propTypes = {
+  openItemModal: PropTypes.func.isRequired,
+  openOutfitModal: PropTypes.func.isRequired,
+  selectAll: PropTypes.func.isRequired,
+  unselectAll: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
+  setFilter: PropTypes.func.isRequired,
+  weatherFilter: PropTypes.string.isRequired,
+  setWeatherFilter: PropTypes.func.isRequired,
+  deleteSelectedItems: PropTypes.func.isRequired,
+  multiSelect: PropTypes.array.isRequired,
+};
+
+export default WardrobeControls;
